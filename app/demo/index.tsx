d19854662c689c3c1a3db9cda3a82ff98ada0e06
sowbharath <b.sakilobm@gmail.com>
2025-05-04 20:50:36 +0530

    Eas Build Changed / Removed Unused Libs

--- app/demo/index.tsx
+++ app/demo/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import { View, StyleSheet, Animated } from 'react-native'
-import Product from '../../components/mug/Product'
-import Ticker from '../../components/mug/Ticker'
-import Circle from '../../components/mug/Circle'
-import Pagination from '../../components/mug/Pagination'
+// import React from 'react';
+// import { View, StyleSheet, Animated } from 'react-native'
+// import Product from '../../components/mug/Product'
+// import Ticker from '../../components/mug/Ticker'
+// import Circle from '../../components/mug/Circle'
+// import Pagination from '../../components/mug/Pagination'
 
-const ProductMugScreen = () => {
-    const scrollX = React.useRef(new Animated.Value(0)).current;
-    return (
-        <View style={styles.container}>
-            <Ticker scrollX={scrollX} />
-            <Circle scrollX={scrollX} />
-            <Product scrollX={scrollX} />
-            <Pagination scrollX={scrollX} />
-        </View>
-    )
-}
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-});
-export default ProductMugScreen;
+// const ProductMugScreen = () => {
+//     const scrollX = React.useRef(new Animated.Value(0)).current;
+//     return (
+//         <View style={styles.container}>
+//             <Ticker scrollX={scrollX} />
+//             <Circle scrollX={scrollX} />
+//             <Product scrollX={scrollX} />
+//             <Pagination scrollX={scrollX} />
+//         </View>
+//     )
+// }
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//     },
+// });
+// export default ProductMugScreen;
